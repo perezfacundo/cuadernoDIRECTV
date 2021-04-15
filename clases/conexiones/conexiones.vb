c2559e39b98c2feef5f14aff8442089054706d69
Facundo Perez <63246299+perezfacundo@users.noreply.github.com>
2021-04-15 15:50:01 -0300

    clase Conexiones

---
+++ clases/conexiones/conexiones.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class conexiones
+    Protected conn As New SqlConnection
+
+    Event Err(descripcion As String)
+
+    Public Function fConectar(Optional CadenaConexion = Nothing)
+        Dim Cadena As String
+
+        If CadenaConexion = Nothing Then
+            Cadena = ConfigurationManager.ConnectionStrings("CLIENTESDIRECTVConnectionString").ConnectionString
+        Else
+            Cadena = CadenaConexion
+        End If
+
+        Try
+            conn = New SqlConnection(Cadena)
+            conn.Open()
+            Return True
+        Catch ex As Exception
+            RaiseEvent Err(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Function fDesconectar()
+
+        Try
+
+            If conn.State = ConnectionState.Open Then
+                conn.Close()
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            RaiseEvent Err(ex.Message)
+            Return False
+        End Try
+
+    End Function
+End Class
--- index.aspx.vb
+++ index.aspx.vb
@@ -1,7 +1,12 @@
-﻿Public Class index
+﻿
+Public Class index
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub cmdLogin_Click(sender As Object, e As EventArgs) Handles cmdLogin.Click
 
     End Sub
 
